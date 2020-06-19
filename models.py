3077ec1dd5b974dba7cd427a12b88741043d7e2e
unknown <magurarm@guilford.edu>
2020-06-19 14:18:12 -0400

    Redo

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 import os
 from sqlalchemy import Column, String, create_engine, Integer
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 # os.environ['DATABASE_URL'] = 'postgres://postgres:1@localhost:5432/herokutest'
 # database_path = os.environ['DATABASE_URL']
-database_path = 'postgres://rbzkztrfwjyghb:96e6932b69f21b236db0420120ac32818cd3d99d9b00f8d1fc6e4a20d8c7bb71@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d75ujhmqfldjfu'
+# database_path = 'postgres://rbzkztrfwjyghb:96e6932b69f21b236db0420120ac32818cd3d99d9b00f8d1fc6e4a20d8c7bb71@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d75ujhmqfldjfu'
 db = SQLAlchemy()
 
 '''
@@ -13,8 +14,8 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://rbzkztrfwjyghb:96e6932b69f21b236db0420120ac32818cd3d99d9b00f8d1fc6e4a20d8c7bb71@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d75ujhmqfldjfu'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object('config')
+    migrate = Migrate(app, db)
     db.app = app
     db.init_app(app)
     db.create_all()
