a211c5c4624caff5f8a4924e2c6faa519f73522c
unknown <magurarm@guilford.edu>
2020-06-19 13:59:56 -0400

    Trying again with database path

--- models.py
+++ models.py
@@ -5,8 +5,7 @@ import json
 
 # os.environ['DATABASE_URL'] = 'postgres://postgres:1@localhost:5432/herokutest'
 # database_path = os.environ['DATABASE_URL']
-database_path = 'postgres://bcjbqauqbtfysh:a525739c097c01944d41b2d063717a62e838f80cd84d3757edb18e05f16863d2@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d73b0l96g8npe4'
-
+database_path = 'postgres://rbzkztrfwjyghb:96e6932b69f21b236db0420120ac32818cd3d99d9b00f8d1fc6e4a20d8c7bb71@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d75ujhmqfldjfu'
 db = SQLAlchemy()
 
 '''
@@ -14,7 +13,7 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://rbzkztrfwjyghb:96e6932b69f21b236db0420120ac32818cd3d99d9b00f8d1fc6e4a20d8c7bb71@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d75ujhmqfldjfu'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
